21b5fbceccf616f24a6afb7fb94d41f88264a506
Documenter.jl <documenter@juliadocs.github.io>
2024-03-20 16:44:57 +0000

    build based on 5259abf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = thesis_code","category":"page"},{"location":"#thesis_code","page":"Home","title":"thesis_code","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for thesis_code.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [thesis_code]","category":"page"}]
+}
