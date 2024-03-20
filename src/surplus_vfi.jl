564b546a74a69f3d724a00ffe23f8a8ca416a968
Bo Jacobs Strom <bojs.13@gmail.com>
2024-03-20 15:45:08 +0100

    removed redundant code in SurplusVFI and commited toml

--- src/surplus_vfi.jl
+++ src/surplus_vfi.jl
@@ -5,7 +5,7 @@ function SurplusVFI(Z, X, Y, Π; MaxIter = 2000, tol = 10e-8, δ = 0.013, r = 0.
 
     for iter in 1:max_iter
         S_next = [s[w, i, j] + ((1 - δ)/(1 + r)) + sum(Π[w, w_prime] * max(S[w_prime, i, j], 0) for w_prime in 1:length(Z)) for w in 1:length(Z), i in 1:length(X), j in 1:length(Y)]
-        e = maximum(abs(S_next - S))
+        
         if maximum(abs, S_next - S) < tol
             @info "The surplus function has succesfully converged after $iter iterations."
             return S_next
